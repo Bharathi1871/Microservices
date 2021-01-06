4b80fad72dc401069257c6f982669c203ca7d064
Bharathi Somana <bharthi.somana@oracle.com>
2021-01-06 16:40:46 +0530

    Added springboot application

---
+++ spring-boot-application/src/main/java/docker/com/example/SpringBootApplicationDkApplication.java
@@ -0,0 +1,13 @@
+package docker.com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootApplicationDkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootApplicationDkApplication.class, args);
+	}
+
+}
